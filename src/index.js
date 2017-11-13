73bef31d6e093585522eddafdb9a1f8990f6bf3c
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-11-13 15:34:02 +0900

    Fixed filter from state to router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import App from './components/App'
+import Root from './components/Root'
 import reducer from './reducers'
 
 const store = createStore(reducer)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 )
