e90e1e09d47f6460f80592ca03440e726397a4c8
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-11-14 13:18:51 +0900

    Added redux-devtools

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import configureStore from '../store/configureStore'
 import App from './App'
+// Don't do this! You’re bringing DevTools into the production bundle.
+import DevTools from '../containers/DevTools';
 
-const Root = ({ store }) => (
+const store = configureStore();
+
+const Root = () => (
   <Provider store={store}>
     <Router>
-      <Route path="/:filter?" component={App} />
+      <div>
+        <Route path="/:filter?" component={App} />
+        <DevTools />
+      </div>
     </Router>
   </Provider>
 )
-
-Root.propTypes = {
-  store: PropTypes.object.isRequired
-}
 
 export default Root
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import Root from './components/Root'
-import reducer from './reducers'
-
-const store = createStore(reducer)
 
 render(
-  <Root store={store} />,
+  <Root />,
   document.getElementById('root')
 )
